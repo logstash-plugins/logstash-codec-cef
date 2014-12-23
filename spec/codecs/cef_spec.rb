8e6244c5a2789037171934b829fbcc57d755452a
Joao Duarte <jsvduarte@gmail.com>
2014-12-23 15:46:24 +0000

    adds tests
    Fixes #4

--- spec/codecs/cef_spec.rb
+++ spec/codecs/cef_spec.rb
@@ -4,52 +4,34 @@ require "logstash/devutils/rspec/spec_helper"
 require "logstash/codecs/cef"
 require "logstash/event"
 
-describe LogStash::Codecs::Cef do
+describe LogStash::Codecs::CEF do
   subject do
-    next LogStash::Codecs::Cef.new
+    next LogStash::Codecs::CEF.new
   end
-=begin
 
   context "#encode" do
-    let (:event) {LogStash::Event.new({"message" => "hello world", "host" => "test"})}
-
-    it "should return a default date formatted line" do
-      expect(subject).to receive(:on_event).once.and_call_original
-      subject.on_event do |e, d|
-        insist {d} == event.to_s + "\n"
-      end
-      subject.encode(event)
-    end
-
-    it "should respect the supplied format" do
-      format = "%{host}"
-      subject.format = format
-      expect(subject).to receive(:on_event).once.and_call_original
-      subject.on_event do |e, d|
-        insist {d} == event.sprintf(format) + "\n"
-      end
-      subject.encode(event)
-    end
+    it "should assert all header fields are present"
   end
 
   context "#decode" do
-    it "should return an event from an ascii string" do
-      decoded = false
-      subject.decode("hello world\n") do |e|
-        decoded = true
+    let (:message) { "CEF:0|security|threatmanager|1.0|100|trojan successfully stopped|10|src=10.0.0.192 dst=12.121.122.82 spt=1232" }
+
+    it "should parse the cef header" do
+      subject.decode(message) do |e|
         insist { e.is_a?(LogStash::Event) }
-        insist { e["message"] } == "hello world"
+        insist { e["cef_version"] } == "0"
+        insist { e["cef_vendor"] } == "security"
+        insist { e["cef_product"] } == "threatmanager"
+        insist { e["cef_device_version"] } == "1.0"
+        insist { e["cef_sigid"] } == "100"
+        insist { e["cef_name"] } == "trojan successfully stopped"
+        insist { e["cef_severity"] } == "10"
+        insist { e["message"] } == "src=10.0.0.192 dst=12.121.122.82 spt=1232"
       end
-      insist { decoded } == true
     end
 
-    it "should return an event from a valid utf-8 string" do
-      subject.decode("München\n") do |e|
-        insist { e.is_a?(LogStash::Event) }
-        insist { e["message"] } == "München"
-      end
-    end
+    it "should parse the cef body"
+    it "should handle values in the body that contain spaces"
   end
 
-=end
 end
